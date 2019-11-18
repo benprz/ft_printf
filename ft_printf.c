b8cdc9913a952b486fba662bdd229535a1ed4bb4
Benjamin Perez <bperez@z1r1p5.le-101.fr>
2019-11-18 20:21:18 +0100

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,29 +6,42 @@
 /*   By: bperez <bperez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 20:12:48 by bperez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/15 21:12:05 by bperez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/18 19:54:49 by bperez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int		ft_printf(const char *str, ...)
+typedef struct 	s_env
+{
+	int			len;
+} 				t_env;
+
+int		parse(const char *format)
+{
+	int len;
+
+	while ()
+}
+
+int		ft_printf(const char *format, ...)
 {
 	va_list args;
+	t_env	printf;
 
-	va_start(args, str);
-	while ((s = va_arg(args, char *)))
-	{
-		printf("%s\n", s);
-	}
+	va_start(args, format);
+	while (*format)
+		format += parse(format);
 	va_end(args);
-	return (0);
+	return (printf.len);
 }
 
 int		main(void)
 {
-	ft_printf("hello", "bonjour", "ohayo", "wena");
+	printf("\n%d\n", ft_printf("Hello %s :)"));
 	return (0);
 }
