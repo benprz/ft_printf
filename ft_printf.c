ee756171d90aabbcffd61deee5246975dd4733cc
Benjamin Perez <bperez@z1r3p4.le-101.fr>
2019-11-19 20:20:47 +0100

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,42 +6,52 @@
 /*   By: bperez <bperez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 20:12:48 by bperez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/18 19:54:49 by bperez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/19 20:20:27 by bperez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-typedef struct 	s_env
-{
-	int			len;
-} 				t_env;
-
-int		parse(const char *format)
+static int	parse_format(const char *format, t_env *env)
 {
 	int len;
 
-	while ()
+	len = 1;
+	if (*format != '%')
+	{
+		write(1, format, 1);
+		env->len++;
+		return (1);
+	}
+	else
+	{
+		write(1, format, 1);
+		env->len++;
+		return (1);
+	}
+	
+	return (len);
 }
 
-int		ft_printf(const char *format, ...)
+int			ft_printf(const char *format, ...)
 {
 	va_list args;
-	t_env	printf;
+	t_env	env;
 
 	va_start(args, format);
-	while (*format)
-		format += parse(format);
+	printf("env->args = %p\n", env.args);
 	va_end(args);
-	return (printf.len);
+	return (env.len);
 }
 
-int		main(void)
+int			main(void)
 {
-	printf("\n%d\n", ft_printf("Hello %s :)"));
+	printf("\nlen = %d\n", ft_printf("Hello %s :)", "Ben"));
 	return (0);
 }
---
+++ ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_printf.h                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: bperez <bperez@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/19 19:17:56 by bperez       #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/19 20:19:27 by bperez      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+typedef struct      s_args
+{
+    void            *arg;
+    char            c;
+    struct s_args   *next;
+}                   t_args;
+
+typedef struct 	    s_env
+{
+    t_args          *args;
+	int             len;
+} 				    t_env;
+
+#endif
