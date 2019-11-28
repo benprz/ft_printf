df91ff06119fe981cdd0c2a47e11923228ba5903
Benjamin Perez <bperez@z1r3p3.le-101.fr>
2019-11-28 19:35:00 +0100

    parse flags width et size fini, gestion de leurs erreurs aussi normalement et tableau de fonctions pour les conversions aussi, plus qu'a coder les fonctions + gérer tous les comportements des flags

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bperez <bperez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 20:12:48 by bperez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/26 20:47:12 by bperez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 19:21:13 by bperez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,88 +20,135 @@
 #include <string.h>
 #include <unistd.h>
 
+void	print_flags(t_args *arg)
+{
+	int i;
+
+	i = 0;
+	printf("\n");
+	while (i < _nbflags)
+	{
+		printf("<flags[%c] = %d>\n", g_flags_directive[i], arg->flags[i]);
+		i++;
+	}
+	printf("<width = %d>\n<size = %d>\n", arg->width, arg->size);
+}
+
 int		is_type(const char c)
 {
 	int i;
 
 	i = 0;
-	while (i < _nbtypes && g_type_conversions[i] != c)
+	while (i < _nbtypes && g_types_conversion_char[i] != c)
 		i++;
-	return (i < _nbtypes);
+	return (i < _nbtypes ? i : ERROR);
 }
 
-int		has_flag(const char c, const char next, int flags[])
+void	skip_digit(const char **format)
+{
+	while (isdigit(**format))
+		(*format)++;
+}
+
+void	get_precision_size(const char **format, t_args *arg)
+{
+	(*format)++;
+	if (*(*format) == g_flags_directive[_wildcard_size])
+		arg->flags[_wildcard_size] = 1;
+	else
+	{
+		arg->size = atoi(*format);
+		skip_digit(format);
+		(*format)--;
+	}
+}
+
+int		has_flag(const char **format, t_args *arg)
 {
 	int i;
 
+	if (isdigit(**format) && **format != g_flags_directive[_zero])
+	{
+		arg->width = atoi(*format);
+		skip_digit(format);
+	}
 	i = 0;
-	printf("<c = %c | next = %c>", c, next);
 	while (i < _nbflags)
 	{
-		if (g_flag_directives[i] == c)
+		if (**format == g_flags_directive[i])
 		{
-			return ((flags[i] = 1));
+			if (**format == g_flags_directive[_precision])
+				get_precision_size(format, arg);
+			return ((arg->flags[i] = 1));
 		}
 		i++;
 	}
 	return (0);
 }
 
-void	init_flags(const char **format, t_args *arg)
+int		init_flags(const char **format, t_args *arg, va_list ap)
 {
 	int i;
 
 	i = 0;
 	while (i < _nbflags)
 		arg->flags[i++] = 0;
-	while (has_flag(**format, *(*format + 1), arg->flags))
+	arg->width = 0;
+	arg->size = 0;
+	while (has_flag(format, arg))
 		(*format)++;
+	if (arg->flags[_wildcard_width] && arg->width)
+		return (ERROR);
+	if (arg->flags[_wildcard_width])
+		arg->width = va_arg(ap, int);
+	if (arg->flags[_wildcard_size])
+		arg->size = va_arg(ap, int);
+	return (1);
 }
 
-int		parse_arg(const char **format, va_list args, int len)
+int		parse_arg(const char **format, va_list ap, int len)
 {
 	t_args	arg;
 
 	(*format)++;
-	init_flags(format, &arg);
-	if (!is_type(**format))
-		return (ERROR);
-	arg.type = *(*format)++;
-	if (arg.type == 'c')
+	if (init_flags(&(*format), &arg, ap) != ERROR)
 	{
-		printf("%c", (unsigned char)va_arg(args, int));
-		return len++;
+		if ((arg.type = is_type(**format)) != ERROR)
+		{
+			len += g_types_conversion_function[arg.type](ap);
+			return (len);
+		}
 	}
-	return (len);
+	return (ERROR);
 }
 
-int		parse_format(const char *format, va_list args)
+int		parse_format(const char *format, va_list ap)
 {
 	int len;
 
 	len = 0;
-	while (*format && len != -1)
+	while (*format && len != ERROR)
 	{
 		if (*format == '%')
-			len = parse_arg(&format, args, len);
+			len = parse_arg(&format, ap, len);
 		else
 		{
 			printf("%c", *format);
 			len++;
-			format++;
 		}
+		format++;
 	}
 	return (len);
 }
 
 int		ft_printf(const char *format, ...)
 {
-	va_list args;
+	va_list ap;
 	int		len;
 
-	va_start(args, format);
-	len = parse_format(format, args);
-	va_end(args);
+	va_start(ap, format);
+	len = parse_format(format, ap);
+	va_end(ap);
 	return (len);
 }
 
@@ -110,11 +157,11 @@ int		main(void)
 	int c;
 
 	printf("[");
-	printf("]\n# len = [%d]\n", ft_printf("Hello %-c", 'b'));
+	printf("]\n# len = [%d]\n", ft_printf("Hello %-010.*c", 9, 'b'));
 
 	printf("\n\n########################\nprintf()");
-	//printf("[%.*f]", -15, 5.156483451534);
-	printf("[%0*+-.2f]", 15, 1.0);
+	//printf("[%.f]", -15, 5.156483451534);
+	printf("[%15.2f]", 1.0);
 	printf("\n########################\n");
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bperez <bperez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/19 19:17:56 by bperez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/26 20:32:44 by bperez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 19:30:06 by bperez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,56 +15,66 @@
 # define FT_PRINTF_H
 
 # define ERROR -1
+#include <stdarg.h>
 
 enum				e_types
 {
 	_char,
 	_digit,
 	_integer,
-	//_octal,
 	_unsigned,
 	_hexa_lowercase,
 	_hexa_uppercase,
-	//_exponent
-	//_float
 	_string,
 	_pointer,
 	_nbtypes
 };
 
-const char			g_type_conversions[_nbtypes] = {
+const char			g_types_conversion_char[_nbtypes] = {
 	'c',
 	'd',
 	'i',
-	//o,
 	'u',
 	'x',
 	'X',
-	//e,
-	//f,
 	's',
 	'p'
+};
+
+int		convert_char(va_list ap);
+int		convert_int(va_list ap);
+//void	convert_unsigned(va_list ap);
+//void	convert_hexa(va_list ap);
+//void	convert_hexa_upper(va_list ap);
+//void	convert_string(va_list ap);
+//void	convert_pointer(va_list ap);
+
+int					(*g_types_conversion_function[_nbtypes])(va_list) = {
+	convert_char,
+	convert_int,
+	convert_int
+	//convert_unsigned,
+	//convert_hexa,
+	//convert_hexa,
+	//convert_string,
+	//convert_pointer
 };
 
 enum				e_flags
 {
 	_minus,
-	//_plus,
 	_zero,
-	//_blank,
-	//_sharp,
 	_wildcard_width,
+	_precision,
 	_wildcard_size,
 	_nbflags
 };
 
-const char			g_flag_directives[_nbflags] = {
+const char			g_flags_directive[_nbflags] = {
 	'-',
-	//'+',
 	'0',
-	//' ',
-	//'#',
 	'*',
+	'.',
 	'*'
 };
 
@@ -73,7 +83,7 @@ typedef struct		s_args
 	int				flags[_nbflags];
 	int				width;
 	int				size;
-	char			type;
+	int				type;
 }					t_args;
 
 #endif
